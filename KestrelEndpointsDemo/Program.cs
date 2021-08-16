3aa9385c2619015fc1a87f8380fb8e1e12d778b5
Yowko Tsai <yowko.tsai@skylinetw.com>
2021-08-16 15:13:19 +0800

    UseKestrel

--- KestrelEndpointsDemo/Program.cs
+++ KestrelEndpointsDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -20,7 +21,12 @@ namespace KestrelEndpointsDemo
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseKestrel(opts =>
+                    {
+                        opts.ListenLocalhost(10000, opts =>opts.Protocols= HttpProtocols.Http1);
+                        opts.ListenLocalhost(10001, opts => opts.UseHttps());
+                    });
+                    // UseUrls 相關設定會被 UseKestrel 覆蓋
                     webBuilder.UseUrls("http://localhost:8000", "https://localhost:8001");
                 });
     }
